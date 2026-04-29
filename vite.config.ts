71f909209bc877792e8f1f13a495ee1f57786485
matekno <mati.rapoport@gmail.com>
2026-04-29 19:24:47 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/taller-majon-visual/' : '/',
-})
+  plugins: [react()]})
